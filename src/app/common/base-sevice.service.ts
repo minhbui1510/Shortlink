65c72a12b41542cb7eaa471b14c53240f83e0170
DESKTOP-VCHS5NN\Bui Hai Minh <buihaiminh.2k@gmail.com>
2022-07-22 23:41:20 +0700

    fix cors

--- src/app/common/base-sevice.service.ts
+++ src/app/common/base-sevice.service.ts
@@ -1,8 +1,52 @@
+import {ApiResultResponse} from "./model/result-response";
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpParams, HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import {environment} from '../../environments/environment';
+import {DEBUG} from "@angular/compiler-cli/src/ngtsc/logging/src/console_logger";
 
 @Injectable({
   providedIn: 'root'
 })
-export class BaseSeviceService {
+export abstract class BaseService {
 
+  private baseUrl = '';
+  private basePath = '';
+
+  constructor() {
+    this.baseUrl = environment.url;
+  }
+
+  abstract getHttp(): HttpClient;
+
+  abstract getServiceName(): string;
+
+  protected doGet(url: string, httpParams?: HttpParams, httpHeaders?: HttpHeaders): Observable<ApiResultResponse> {
+    const requestUrl =`${this.baseUrl}${this.basePath}${url}`;
+    return this.getHttp().get<ApiResultResponse>(requestUrl, { headers: httpHeaders, params: httpParams });
+  }
+
+  protected doPost(url: string, body: any, httpParams?: HttpParams, httpHeaders?: HttpHeaders): Observable<ApiResultResponse> {
+    const requestUrl =`${this.baseUrl}${this.basePath}${url}`;
+    return this.getHttp().post<ApiResultResponse>(requestUrl, body || {}, {
+      headers: httpHeaders,
+      params: httpParams
+    });
+  }
+
+  protected postDataBlob(url: string, body: any, header?: HttpHeaders, inputParams?: HttpParams): Observable<any> {
+    const requestUrl =`${this.baseUrl}${this.basePath}${url}`;
+    return this.getHttp().post<any>(requestUrl, body, { headers: header, params: inputParams, responseType: 'blob' as 'json' });
+  }
+
+  protected doDelete(url: string, httpParams?: HttpParams, httpHeaders?: HttpHeaders) {
+    const requestUrl =`${this.baseUrl}${this.basePath}${url}`;
+    return this.getHttp().delete<ApiResultResponse>(requestUrl, { headers: httpHeaders, params: httpParams });
+  }
+
+  public logDebug(value: any) {
+    if (!environment.production) {
+      // console.log(${this.getServiceName()} [DEBUG]:, value);
+    }
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,8 @@
 
 export const environment = {
   production: false,
-  url: 'localhost:8080/'
+  url: 'http://localhost:8080/'
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
