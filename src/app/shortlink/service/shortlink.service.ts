fad1d9451607c16d2e5e77f77bcbdc83ac732e45
DESKTOP-VCHS5NN\Bui Hai Minh <buihaiminh.2k@gmail.com>
2022-07-23 00:25:33 +0700

    api do check

--- src/app/shortlink/service/shortlink.service.ts
+++ src/app/shortlink/service/shortlink.service.ts
@@ -3,16 +3,27 @@ import { HttpClient } from "@angular/common/http";
 import { LongUrlRequest } from '../model/LongUrlRequest';
 import { environment } from 'src/environments/environment';
 import { map, Observable } from 'rxjs';
+import {BaseService} from "../../common/base-sevice.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class ShortlinkService {
+export class ShortlinkService extends BaseService{
   constructor(
     private http: HttpClient,
   ) {
+    super();
   }
   doCheckLink(body: LongUrlRequest): Observable<any> {
-    return this.http.post<LongUrlRequest>(`${environment.url}shortURL`, body).pipe(map(response => response ? response : {}));
+    // return this.http.post<LongUrlRequest>(`${environment.url}shortURL`, body).pipe(map(response => response ? response : {}));
+    return  this.doPost(this.getServiceName(), body);
+  }
+
+  getHttp(): HttpClient {
+    return this.http;
+  }
+
+  getServiceName(): string {
+    return "shortURL";
   }
 }
